92bdcea23e9de24bc5956bc0c80166f668e6a776
Rakshit Rawal <apple@Mac.lan>
2026-05-06 05:23:48 +0530

    Fix socket env issue

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,8 +1,13 @@
 import { io } from "socket.io-client";
 
-const socket = io(process.env.REACT_APP_API_URL.replace("/api", ""), {
+const API_URL =
+  process.env.REACT_APP_API_URL ||
+  "http://localhost:5001/api";
 
+const SOCKET_URL = API_URL.replace("/api", "");
+
+const socket = io(SOCKET_URL, {
   transports: ["websocket"],
-
 });
+
 export default socket;
